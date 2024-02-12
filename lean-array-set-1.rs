b918ef3f12fd735142fae45cc5f95f8c26f1784f
muqsit-azeem <muqsit@amazon.com>
2024-02-12 15:04:06 -0800

    one array example

---
+++ lean-array-set-1.rs
@@ -0,0 +1,13 @@
+fn set_elements(mut arr: kani::array::Array<i32>, mut index:usize) -> kani::array::Array<i32> {
+    if index < arr.len() {
+        arr[index] = 1;
+        return set_elements(arr, index + 1);
+    } else {
+	return arr;
+    }
+}
+
+
+pub fn set_one(mut arr: kani::array::Array<i32>) -> kani::array::Array<i32> {
+    set_elements(arr, 0)
+}
